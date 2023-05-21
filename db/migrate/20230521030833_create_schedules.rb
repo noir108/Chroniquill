c5aada042189e2c64d397bd40f396d1de5d6b5ae
noir108 <tymbsd10@gmail.com>
2023-05-21 13:35:35 +0900

    テーブル作成

--- db/migrate/20230521030833_create_schedules.rb
+++ db/migrate/20230521030833_create_schedules.rb
@@ -6,8 +6,8 @@ class CreateSchedules < ActiveRecord::Migration[6.0]
       t.text :description,        null: true
       t.datetime :start_time,		  null: false
       t.datetime :end_time,     	null: true
-      t.references :user,         null: false, foreign_key: true
-      t.references :category,     null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
       t.timestamps
     end
   end
