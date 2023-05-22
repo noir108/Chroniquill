0f4defe949f824df5274564504a3c171db94c0b7
noir108 <tymbsd10@gmail.com>
2023-05-22 18:17:58 +0900

    テストコードの実行

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,6 +3,19 @@ class Schedule < ApplicationRecord
   with_options presence: true do
     validates :title, :start_time
   end
+  validate :start_time_before_end_time,:end_time_after_start_time
+
+  def start_time_before_end_time  #start_timeがend_timeより後の日付だとバツ
+    if start_time && end_time && start_time > end_time
+      errors.add(:start_time, "must be before end time")
+    end
+  end
+
+  def end_time_after_start_time   #end_timeがstart_timeより前の日付だとバツ
+    if start_time && end_time && end_time < start_time
+      errors.add(:end_time, "must be after start time")
+    end
+  end
 
   belongs_to :user
 end
