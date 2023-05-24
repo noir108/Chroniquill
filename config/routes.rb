746ca32687e1194e942cf12006bbfdab85033c29
noir108 <tymbsd10@gmail.com>
2023-05-24 12:11:07 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'schedules#index'
   resources :schedules, only: [:index, :new, :create, :edit, :update, :destroy]
+  get '/schedules/index2', to: 'schedules#index2', as: 'index2_schedules'
 end
