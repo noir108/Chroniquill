f534af6f94adecfd9fc14b568d65409c5cb7f616
noir108 <tymbsd10@gmail.com>
2023-05-25 10:57:08 +0900

    Full calendarの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,7 @@
 import $ from 'jquery';
 import Rails from '@rails/ujs';
+import 'fullcalendar';
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
