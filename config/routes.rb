6c03d841780a5808748e62b6b83128cea48ca7d0
noir108 <tymbsd10@gmail.com>
2023-05-26 16:26:36 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'schedules#index'
   resources :schedules, only: [:index, :new, :create, :edit, :update, :destroy]
   get '/schedules/index2', to: 'schedules#index2', as: 'index2_schedules'
-  resources :categorys, only: [:index, :new, :create, :edit, :update, :destory]
+  resources :categorys, only: [:index, :new, :create, :edit, :update, :destroy]
 end
