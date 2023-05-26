e9aa906c5dd41cf273f3c6f015f34ed6ab83841b
noir108 <tymbsd10@gmail.com>
2023-05-26 12:16:56 +0900

    スクロールの実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../calendar")
+require("../scroll")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
