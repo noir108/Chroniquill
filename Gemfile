8bce78bcabdf68d10a0d2b35434933de3ecb7cb8
noir108 <tymbsd10@gmail.com>
2023-06-20 11:37:06 +0900

    momentjs-railsの導入

--- Gemfile
+++ Gemfile
@@ -66,3 +66,5 @@ end
 
 gem 'devise'
 gem 'jquery-rails'
+gem 'momentjs-rails'
+gem 'fullcalendar-rails'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,3 @@
-import $ from 'jquery';
-import Rails from '@rails/ujs';
-import 'fullcalendar';
-
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -11,6 +7,10 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../calendar")
+
+//= require moment
+//= require fullcalendar
+//= require jquery
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
